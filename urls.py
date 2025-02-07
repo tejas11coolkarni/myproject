b727ac3300dff61a68b5a602881326e16a986a12
tejas11coolkarni <73683210+tejas11coolkarni@users.noreply.github.com>
2025-02-07 16:08:37 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myproject import views
+# this is comment
 
 urlpatterns = [
     path('admin/', admin.site.urls),
